bfe483a4ab0307e42003a5e15112cc277615649e
meme-d0ge <MEME_D0GE@proton.me>
2025-01-23 14:37:33 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -21,9 +22,9 @@ async function bootstrap() {
       {
         type: 'http',
         scheme: 'bearer',
-        bearerFormat: 'JWT', // Можно указать формат токена (например, JWT)
+        bearerFormat: 'JWT',
       },
-      'access-token', // Название схемы авторизации
+      'access-token',
     )
     .build();
   const document = SwaggerModule.createDocument(app, config);
